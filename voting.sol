950c75291b58583fc49931bde14eaf2a8a18a7bf
Tanishq Singh <singhtanishq0210@gmail.com>
2024-04-16 07:12:11 +0530

    Add files via upload
    Smart Contract

---
+++ voting.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.8.19;
+
+contract Voting {
+    struct Candidate {
+        string name;
+        uint votes;
+    }
+    Candidate[] public candidates;
+    address owner;
+    string public position;
+    mapping (bytes32 => bool) public voters;
+    uint public votingEndDateTime;
+
+
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function addCandidate(string memory candidateName) public onlyOwner {
+        candidates.push(Candidate({
+                name: candidateName,
+                votes: 0
+        }));
+    }
+
+    function vote(uint candidateIndex) public {
+        require(!voters[keccak256(abi.encodePacked(msg.sender))], "You have already voted.");
+        require(candidateIndex < candidates.length, "Invalid candidate index.");
+
+        candidates[candidateIndex].votes++;
+        voters[keccak256(abi.encodePacked(msg.sender))] = true;
+    }
+
+    function getAllVotesOfCandiates() public view returns (Candidate[] memory) {
+        return candidates;
+    }
+
+    function getVotingStatus() public view returns (bool) {
+        return (block.timestamp < votingEndDateTime);
+    }
+}
